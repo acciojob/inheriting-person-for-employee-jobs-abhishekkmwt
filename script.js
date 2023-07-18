060fd9602be2e829749d001517aa5430a87df8ef
Abhishek Kumawat <65283365+abhishekkmwt@users.noreply.github.com>
2023-07-18 13:31:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 	
 }
 Person.prototype.greet=function() {
-	console.log('Hello, my name is '+this.name+', I am '+this.age+' years old')
+	console.log('Hello, my name is '+this.name+' and I am '+this.age+' years old.')
 }
 
 function Employee(name, age, jobTitle) {
@@ -13,7 +13,7 @@ function Employee(name, age, jobTitle) {
 	this.jobTitle=jobTitle;
 }
 Employee.prototype.jobGreet=function() {
-	console.log('Hello, my name is '+this.name+', I am '+this.age+' years old, and my job title is '+this.jobTitle)
+	console.log('Hello, my name is '+this.name+', I am '+this.age+' years old, and my job title is '+this.jobTitle+'.')
 
 }
 Employee.prototype.__proto__=Person.prototype;
